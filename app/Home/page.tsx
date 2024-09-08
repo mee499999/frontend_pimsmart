4e0d5e6c62d85ee66e7f20e2ac2dd4733757dfec
Suhaimee kaje <111553531+Suhaimee123@users.noreply.github.com>
2024-09-08 22:50:20 +0700

    edit main page

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -2,10 +2,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { getUser } from '../api/test';
-import { User } from '@/types/IResponse';
-import Navbar from '@/components/Navbar';
-import Table from '@/components/Table';
+
 import Layout from '@/components/Layout';
 
 const Page: React.FC = () => {
--- components/Content.tsx
+++ components/Content.tsx
@@ -1,19 +1,27 @@
-import { styled } from "@mui/material/styles";
+import React from 'react';
+import { styled } from '@mui/material/styles';
 
 const StyledContent = styled('div')(({ theme }) => ({
   fontFamily: theme.typography.fontFamily,
   fontWeight: theme.typography.fontWeightRegular,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%', // ใช้ความสูงที่ต้องการ
 }));
 
 const StyledTitle = styled('h1')(({ theme }) => ({
   fontFamily: theme.typography.fontFamily,
   fontWeight: theme.typography.fontWeightBold,
+  textAlign: 'center', // เพิ่มการจัดตำแหน่งข้อความให้ตรงกลาง
+  marginBottom: '20px', // เพิ่มระยะห่างด้านล่างของ title
 }));
 
-const Content: React.FC<{ title: string; children: React.ReactNode }> = ({ title, children }) => {
+const Content: React.FC<{ title?: string; children: React.ReactNode }> = ({ title, children }) => {
   return (
     <StyledContent>
-      <StyledTitle>{title}</StyledTitle>
+      {title && <StyledTitle>{title}</StyledTitle>} {/* Only render the title if it exists */}
       <div>{children}</div>
     </StyledContent>
   );
