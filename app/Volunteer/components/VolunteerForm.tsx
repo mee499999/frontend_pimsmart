bac8f6ba12664f2960776fa1c99ce35c6245a1b1
asmannarak\asman8 <asman.saeputa8888@gmail.com>
2024-09-13 23:32:44 +0700

    clean code Volunteer and check Hours

--- app/Volunteer/components/VolunteerForm.tsx
+++ app/Volunteer/components/VolunteerForm.tsx
@@ -53,8 +53,12 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
 
     try {
       const adjustedFormValues = {
-        studentId: formValues.studentId1, // Rename the key here
+        studentId: formValues.studentId1,
+        title: formValues.title, // เพิ่มฟิลด์นี้สำหรับคำนำหน้า
         firstName: formValues.firstName,
+        nickname: formValues.nickname,
+        graduete: formValues.graduete,
+        branch: formValues.branch,
         activityName: formValues.activityName,
         organizationName: formValues.organizationName,
         organizationPhone: formValues.organizationPhone,
@@ -62,6 +66,9 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
         activityDate: formValues.activityDate,
         hours: formValues.hours,
         createDate: formValues.createDate,
+        yearLevel: formValues.yearLevel,
+        loanStatus: formValues.loanStatus
+
       };
 
       await onSubmit(adjustedFormValues); // Ensure onSubmit is a promise-based function
@@ -70,21 +77,25 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
       localStorage.removeItem('volunteerForm');
       setFormValues({
         studentId1: '',
+        title: '', // เคลียร์ค่านี้ด้วย
         firstName: '',
+        yearlevel: '',
+        nickname: '',
+        graduete: '',
+        branch: '',
         activityName: '',
         organizationName: '',
         organizationPhone: '',
         activityDescription: '',
         activityDate: '',
         hours: '',
+        loanStatus:'',
         createDate: new Date().toISOString().slice(0, 10),
       });
 
       // Display success message
       setSuccessMessage("Form submitted successfully!");
 
-      // Optionally, redirect or reload
-      // window.location.reload(); // or use router.push('/confirmation');
     } catch (error) {
       setError('An error occurred while submitting the form. Please try again.');
     } finally {
@@ -113,6 +124,26 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
               helperText={studentIdError ? 'Please enter only numbers' : ''}
             />
           </Grid>
+
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              select
+
+              name="title"
+              value={formValues.title}
+              onChange={handleChange}
+              SelectProps={{
+                native: true,
+              }}
+            >
+              <option value="">เลือกคำนำหน้า</option>
+              <option value="นาย">นาย</option>
+              <option value="นางสาว">นางสาว</option>
+              <option value="นาง">นาง</option>
+            </TextField>
+          </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
@@ -125,34 +156,133 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
-              label="ชื่อกิจกรรมที่ทำ"
+              label="ชื่อเล่น"
+              name="nickname"
+              value={formValues.nickname}
+              onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              select
+
+              name="yearlevel"
+              value={formValues.yearlevel}
+              onChange={handleChange}
+              SelectProps={{
+                native: true,
+              }}
+            >
+              <option value="">ชั้นปี</option>
+              <option value="นักศึกษาชั้นปีที่1">นักศึกษาชั้นปีที่1</option>
+              <option value="นักศึกษาชั้นปีที่2">นักศึกษาชั้นปีที่2</option>
+              <option value="นักศึกษาชั้นปีที่3">นักศึกษาชั้นปีที่3</option>
+              <option value="นักศึกษาชั้นปีที่4">นักศึกษาชั้นปีที่4</option>
+              <option value="นักศึกษาชั้นปีที่5">นักศึกษาชั้นปีที่5</option>
+            </TextField>
+          </Grid>
+
+
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              select
+              label="เลือกสาขา"
+              name="branch"
+              value={formValues.branch}  // เช็คให้แน่ใจว่าใน formValues มี field 'branch'
+              onChange={handleChange}
+              SelectProps={{
+                native: true,
+              }}
+            >
+              <option value="">เลือกสาขา</option> {/* Default value */}
+              <option value="MTM">MTM</option>
+              <option value="IMTM">IMTM</option>
+              <option value="FBM">FBM</option>
+              <option value="RBM">RBM</option>
+              <option value="LTM">LTM</option>
+              <option value="BC">BC</option>
+              <option value="BJ">BJ</option>
+              <option value="CEB">CEB</option>
+              <option value="CB">CB</option>
+              <option value="CJ">CJ</option>
+              <option value="DIT">DIT</option>
+              <option value="CAI">CAI</option>
+              <option value="IE">IE</option>
+              <option value="AME">AME</option>
+              <option value="RAE">RAE</option>
+              <option value="IAM">IAM</option>
+              <option value="AVI">AVI</option>
+              <option value="HTM">HTM</option>
+              <option value="RPM">RPM</option>
+              <option value="HROM">HROM</option>
+              <option value="FTM">FTM</option>
+              <option value="PTM">PTM</option>
+              <option value="TCL">TCL</option>
+              <option value="ELT">ELT</option>
+              <option value="NS">NS</option>
+              <option value="HIT">HIT</option>
+            </TextField>
+          </Grid>
+
+
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              select
+              label="กู้กยศ. หรือไม่" // Label for the dropdown
+              name="loanStatus" // Name for the form field
+              value={formValues.loanStatus} // Make sure this matches your formValues
+              onChange={handleChange}
+              SelectProps={{
+                native: true,
+              }}
+            >
+              <option value="">กู้กยศ. หรือไม่</option> {/* Default value */}
+              <option value="yes">กู้กยศ.</option>
+              <option value="no">ไม่กู้กยศ.</option>
+            </TextField>
+          </Grid>
+
+
+
+
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              label="ชื่อกิจกรรมจิตอาสา"
               name="activityName"
               value={formValues.activityName}
               onChange={handleChange}
             />
           </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
-              label="ชื่อองค์กร"
+              label="หน่วยงาน/เจ้าของงานจิตอาสา"
               name="organizationName"
               value={formValues.organizationName}
               onChange={handleChange}
             />
           </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
-              label="เบอร์โทร องค์กร"
+              label="เบอร์โทรศัพท์ หน่วยงาน/เจ้าของงานจิตอาสา (ถ้ามี)"
               name="organizationPhone"
               value={formValues.organizationPhone}
               onChange={handleChange}
             />
           </Grid>
+
           <Grid item xs={12}>
             <TextField
               fullWidth
-              label="รายละเอียดจิตอาสา"
+              label="รายละเอียดจิตอาสาที่ทำ อธิบาย"
               name="activityDescription"
               value={formValues.activityDescription}
               onChange={handleChange}
@@ -160,10 +290,11 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
               rows={4}
             />
           </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
-              label="วันที่-จัดทำกิจกรรม"
+              label="วันที่จัดทำกิจกรรม"
               name="activityDate"
               value={formValues.activityDate}
               onChange={handleChange}
@@ -171,6 +302,7 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
               InputLabelProps={{ shrink: true }}
             />
           </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
@@ -181,6 +313,7 @@ const VolunteerForm: React.FC<VolunteerFormProps> = ({
               type="number"
             />
           </Grid>
+
           <Grid item xs={12} md={6}>
             <TextField
               fullWidth
